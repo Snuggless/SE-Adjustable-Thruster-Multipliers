bd281b0577e14049f780187e80593134121bd6bf
Jay ( aka Snugg ) <58424597+Snuggless@users.noreply.github.com>
2025-01-01 16:53:11 +0000

    Add thruster specific values to Settings.cs

--- AdjustableThrusterMultipliers/Settings.cs
+++ AdjustableThrusterMultipliers/Settings.cs
@@ -2,12 +2,29 @@
 {
     public class Settings
     {
-        public float MaxThrustMultiplier { get; set; } = 100f;
-        public float FuelUsePerMultiplier { get; set; } = 2f;
+        public ThrusterSettings GlobalThrusters { get; set; }
+        public ThrusterSettings IonThruster { get; set; }
+        public ThrusterSettings HydrogenThruster { get; set; }
+        public ThrusterSettings AtmosphericThruster { get; set; }
+        
         public string[] BlacklistedThrustSubtypes { get; set; } = new string[] { "ThrustSubtypeNameGoesHere" };
+
+        public Settings()
+        {
+            GlobalThrusters = new ThrusterSettings();
+            IonThruster = new ThrusterSettings();
+            HydrogenThruster = new ThrusterSettings();
+            AtmosphericThruster = new ThrusterSettings();
+        }
 
         public Settings LoadSettings() => new Settings();
 
         public void SaveSettings() {  }
+
+        public class ThrusterSettings
+        {
+            public float MaxThrustMultiplier { get; set; } = 11f;
+            public float FuelUsePerMultiplier { get; set; } = 2.5f;
+        }
     }
 }
